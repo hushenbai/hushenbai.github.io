45ab039ff83722054f1723a504562ffac896ad8d
Yaksha0824 <foxxiuxiu@gmail.com>
2025-02-01 13:25:08 +0800

    rsm头图动画

--- rsm/rsm.js
+++ rsm/rsm.js
@@ -46,8 +46,4 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 });
 
-document.querySelector('.scroll-container').addEventListener('animationend', function() {
-    this.style.animation = 'none';
-    this.offsetHeight; // 触发回流
-    this.style.animation = 'scroll 60s linear infinite';
-});
+const container = document.querySelector('.scroll-container');
