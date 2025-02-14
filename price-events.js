54bd55adb92f1604b161ad82e921a90252a73751
Yaksha0824 <foxxiuxiu@gmail.com>
2025-02-14 16:37:55 +0800

    微调

--- price-events.js
+++ price-events.js
@@ -92,3 +92,25 @@ function updateArtworkCoefficient(artwork) {
         artwork.coefficient = latestCoefficient;
     }
 } 
+
+function getArtworkCoefficient(serialnumber, date) {
+    // 如果提供了日期，获取该日期之前的最新系数
+    if (date) {
+        const eventsBeforeDate = priceEvents.filter(event => 
+            event.serialnumber === serialnumber && 
+            new Date(event.date) <= date
+        );
+        
+        if (eventsBeforeDate.length > 0) {
+            const latestEvent = eventsBeforeDate.sort((a, b) => 
+                new Date(b.date) - new Date(a.date)
+            )[0];
+            return latestEvent.coefficient;
+        }
+    }
+    
+    // 如果没有日期或没有找到事件，从原始数据中获取默认系数
+    const allArtworks = Object.values(groupedArtworks).flat();
+    const artwork = allArtworks.find(p => p.serialnumber === serialnumber);
+    return artwork ? artwork.coefficient : 1;
+}
